9b3792862f6fb18208f3d1cda8c0d444761091bb
rohit james <rohitjames77@outlook.com>
2024-01-11 09:09:12 +0530

    Seperate Gridselection div

--- main.js
+++ main.js
@@ -1,23 +1,63 @@
+// function for customizing background color as per button selection ................
+let selectionButtons = document.querySelector('.Control-Panel');
+selectionButtons.addEventListener('click', buttonsInput )
 
-
-let screen = document.querySelector('.screen1');
-let screenHeight = screen.style.height;
-console.log(screenHeight);
-let num;
-
-function createGrid (num){
-    let numSquare =  num*num;
-    console.log(numSquare);
-for (let i =0; i<= numSquare;i++){
-    let screenChild = document.createElement('div');
-    screenChild.className = 'screenChild';
-    screen.append(screenChild);
- screenChild.style.height = screen.clientHeight/num+'px';
- screenChild.style.width = screen.clientWidth/num+'px';
+function buttonsInput (event) {
+    if (event.target.className == "black" ){
+        defaultBackground = '#000000';
+    }else if (event.target.className == 'color-selection'){
+        defaultBackground = event.target.value; 
+    }
+    else if (event.target.className == 'erase'){
+        defaultBackground = '#FFFFFF';
     }
 }
+// Grid Selection Event ........................................................
 
-createGrid(64);
+
+
+
+// function for Div Grid Creation ...............................................
+
+let gridInput;
+let gridSelectionDiv = document.querySelector('.gridSelection');
+gridSelectionDiv.addEventListener('click',createGrid())
+
+ 
+let screen = document.querySelector('.screen1');
+let screenHeight = screen.style.height;
+
+// Event function ....................................................................
+function createGrid (event){
+    if (event.target.className == 'smallGrid'){
+        gridInput = 64;
+        console.log(gridInput);
+    }else if (event.target.className == 'mediumGrid'){
+        gridInput= 32;
+    console.log(gridInput);
+    }
+    else if (event.target.className == 'largeGrid'){
+        gridInput = 18;
+        console.log(gridInput);
+    }
+
+    let numSquare = gridInput * gridInput;
+    let fragment = document.createDocumentFragment();
+    
+    for (let i = 0; i < numSquare; i++) {
+        let screenChild = document.createElement('div');
+        screenChild.className = 'screenChild';
+        fragment.appendChild(screenChild);
+
+        screenChild.style.height = screen.clientHeight / gridInput + 'px';
+        screenChild.style.width = screen.clientWidth / gridInput + 'px';
+    }    
+
+    screen.appendChild(fragment);
+}    
+
+
+// Event listener for mouseHover effect.............................................
 
 let allChildDiv= document.querySelectorAll('.screenChild');
 
@@ -25,24 +65,10 @@ let allChildDiv= document.querySelectorAll('.screenChild');
 allChildDiv.forEach(function (allChildDiv){
     allChildDiv.addEventListener('mouseover', function (){
         allChildDiv.style.backgroundColor = defaultBackground;
-    })
+    })    
     
-})
+})    
 
-let selectionButtons = document.querySelector('.Control-Panel');
-selectionButtons.addEventListener('click', buttonsInput )
 
 let defaultBackground;
-function buttonsInput (event) {
-if (event.target.className == "black" ){
-    defaultBackground = '#000000';
-}else if (event.target.className == 'color-selection'){
-    defaultBackground = event.target.value; 
-}
-else if (event.target.className == 'erase'){
-    defaultBackground = '#FFFFFF';
-    
-}
-
-}
 
