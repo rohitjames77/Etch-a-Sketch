8005d98e14fdb8de435c2cb7349bf7c0a8738783
rohit james <rohitjames77@outlook.com>
2024-01-13 12:30:03 +0530

    fix grid selection reload issue

--- main.js
+++ main.js
@@ -1,48 +1,49 @@
-document.addEventListener('DOMContentLoaded', function () {
 let screen = document.querySelector('.screen1');
-let screenHeight = screen.style.height;
-console.log(screenHeight);
-let gridSelectionDiv = document.querySelector('.gridSelection');
-gridSelectionDiv.addEventListener('click',demo);
-function demo (event){
+let defaultBackground;
+document.addEventListener('DOMContentLoaded', function () {
     let gridInput;
-    if (event.target.className == 'smallGrid'){
-                gridInput = 64;
-                
-            }else if (event.target.className == 'mediumGrid'){
-                    gridInput= 32;
-           
-                }
-                else if (event.target.className == 'largeGrid'){
-                        gridInput = 18;
-                       
-                    }
-                   
-                    createGrid (gridInput);
-    return gridInput;                
-        
-    
+
+    let gridSelectionDiv = document.querySelector('.gridSelection');
+    gridSelectionDiv.addEventListener('click', demo);
+
+    function demo(event) {
+        if (event.target.className == 'smallGrid') {
+            gridInput = 64;
+        } else if (event.target.className == 'mediumGrid') {
+            gridInput = 32;
+        } else if (event.target.className == 'largeGrid') {
+            gridInput = 18;
+        }
+
+
+        clearGrid();
+        createGrid(gridInput);
     }
 
+    function clearGrid() {
+        while (screen.firstChild) {
+            screen.removeChild(screen.firstChild);
+        }
+    }
 
- function createGrid (gridInput){
-                    let screen = document.querySelector('.screen1');            
+    function createGrid(gridInput) {
         let numSquare = gridInput * gridInput;
         let fragment = document.createDocumentFragment();
-    
+
         for (let i = 0; i < numSquare; i++) {
             let screenChild = document.createElement('div');
             screenChild.className = 'screenChild';
             fragment.appendChild(screenChild);
-    
+
             screenChild.style.height = screen.clientHeight / gridInput + 'px';
             screenChild.style.width = screen.clientWidth / gridInput + 'px';
-        }    
-    
-        screen.appendChild(fragment);
-    } 
-    
+        }
 
+        screen.appendChild(fragment);
+    }
+
+
+});
 
     screen.addEventListener('mouseover', function (event) {
         if (event.target.classList.contains('screenChild')) {
@@ -52,94 +53,19 @@ function demo (event){
 
 
     let selectionButtons = document.querySelector('.Control-Panel');
-let defaultBackground;
 selectionButtons.addEventListener('click', buttonsInput )
 function buttonsInput (event) {
 if (event.target.className == "black" ){
     defaultBackground = '#000000';
+    console.log(defaultBackground);
 }else if (event.target.className == 'color-selection'){
-    defaultBackground = event.target.value; 
+    defaultBackground = event.target.value;
+    console.log(defaultBackground); 
 }
 else if (event.target.className == 'erase'){
     defaultBackground = '#FFFFFF';
+    console.log(defaultBackground);
 
 }
 
-}
-});
-// // function for customizing background color as per button selection ................
-
-// let defaultBackground;
-// let selectionButtons = document.querySelector('.Control-Panel');
-// selectionButtons.addEventListener('click', buttonsInput )
-
-// function buttonsInput (event) {
-//     if (event.target.className == "black" ){
-//         defaultBackground = '#000000';
-//         console.log(defaultBackground);
-//     }else if (event.target.className == 'color-selection'){
-//         defaultBackground = event.target.value; 
-//         console.log(defaultBackground);
-//     }
-//     else if (event.target.className == 'erase'){
-//         defaultBackground = '#FFFFFF';
-//         console.log(defaultBackground);
-//     }
-// }
-// // Event listener for mouseHover effect.............................................
-
-// let allChildDiv= document.querySelectorAll('.screenChild');
-// allChildDiv.forEach(function (allChildDiv){
-//     allChildDiv.addEventListener('mouseover', function (){
-//      allChildDiv.style.backgroundColor = defaultBackground;
-//     })    
-    
-// })    
-// // function for Div Grid Creation ...............................................
-
-// 
-// let gridSelectionDiv = document.querySelector('.gridSelection');
-// console.log(gridSelectionDiv);
-// 
-// function demo (event){
-// if (event.target.className == 'smallGrid'){
-//             gridInput = 64;
-//             console.log(gridInput);
-//         }else if (event.target.className == 'mediumGrid'){
-//                 gridInput= 32;
-//             console.log(gridInput);
-//             }
-//             else if (event.target.className == 'largeGrid'){
-//                     gridInput = 18;
-//                     console.log(gridInput);
-//                 }
-//                 
-    
-
-// }
-
- 
-
-// // gridSelection Event function ....................................................................
-// // function createGrid (gridInput){
-// //                 let screen = document.querySelector('.screen1');            
-// //     let numSquare = gridInput * gridInput;
-// //     let fragment = document.createDocumentFragment();
-    
-// //     for (let i = 0; i < numSquare; i++) {
-// //         let screenChild = document.createElement('div');
-// //         screenChild.className = 'screenChild';
-// //         fragment.appendChild(screenChild);
-
-// //         screenChild.style.height = screen.clientHeight / gridInput + 'px';
-// //         screenChild.style.width = screen.clientWidth / gridInput + 'px';
-// //     }    
-
-// //     screen.appendChild(fragment);
-// // } 
-// // createGrid(64) ;  
-
-
-
-
-
+};
